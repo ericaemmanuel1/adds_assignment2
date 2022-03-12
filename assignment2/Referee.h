8a9b60b66e4d1c04dc960e0c5694712a913022a2
ericaemmanuel1 <83539543+ericaemmanuel1@users.noreply.github.com>
2022-03-12 20:06:41 +1030

    Update Referee.h

--- assignment2/Referee.h
+++ assignment2/Referee.h
@@ -1,12 +1,13 @@
-#ifndef REFREE_H
-#define REFREE_H
-#include <string>
-#include <iostream>
+#ifndef REFEREE_H
+#define REFEREE_H
 
-class Refree
-    public:
-    Referee( ); // constructor
-    char refGame(Human player1, Computer player2)// returns the outcome for player1:  'W', 'L' or 'T' (Win, Lose, Tie)
+#include "Computer.h"
+#include "Human.h"
+
+class Referee 
+{    
+	public:        
+		char refGame(HumanPlayer p1,ComputerPlayer p2);
 };
 
 #endif
